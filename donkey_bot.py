a12197329d126c1d50ac839877b8abb5b7551fe5
Michael <thesentientgelatin@gmail.com>
2019-09-11 00:36:19 -0700

    Added CSGO RSS to +get
    Added CSGO functionality to +get command,
    started experiments with playing sound (failures but ive left it in the code for my nighly commit)
    Also tweaked multireplace() to enable use of second parameter.

--- donkey_bot.py
+++ donkey_bot.py
@@ -1,17 +1,23 @@
 import discord
 from discord.ext import commands
+from discord.voice_client import VoiceClient
+from discord.utils import get
 import feedparser
+import html
+
 token = open("token.txt","r").read()
 
 prefix = "+"
 #client = discord.Client()
+vclient = discord.VoiceClient
+guild = discord.Guild
+
 
 bot = commands.Bot(command_prefix=prefix) 
-tf2feed = feedparser.parse('http://www.teamfortress.com/rss.xml')
 
 def multireplace(r, s, t):
 	for i in r:
-		t = t.replace(i, "")
+		t = t.replace(i, s)
 	return t
 
 @bot.event
@@ -43,11 +49,42 @@ async def say(ctx, *, content:str):
 @bot.command()
 async def get(ctx, game:str):
 		if game == "TF2":
-			for i in tf2feed.entries:
+			feed = feedparser.parse('http://www.teamfortress.com/rss.xml')
+			for i in feed.entries:
 				if i.title == "Team Fortress 2 Update Released":
 					await ctx.send(i.link)                  
 					await ctx.send(i.title + " " + i.published)
 					await ctx.send(multireplace(["<ul>", "<li>", "</ul>", "<br/>"], "", i.description))
 					break
+		elif game == "CSGO":
+			feed = feedparser.parse('http://blog.counter-strike.net/index.php/category/updates/feed/')
+			i = feed.entries[0] #for i in feed.entries:
+			await ctx.send(i.link)                  
+			await ctx.send(i.title + " " + i.published)
+			await ctx.send(multireplace(["<p>", "</p>", "<br />"],"", html.unescape(i.content[0].value)))
+					
+@bot.command()
+async def join(ctx):
+	channel = ctx.message.author.voice.channel
+	await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+	channel = ctx.message.author.voice.channel
+	voice = ctx.guild.voice_client
+	await voice.disconnect()
+
+# @bot.command()
+# async def playsound(ctx):
+	# user = ctx.message.author
+	# voice_channel = user.voice.channel
+	# channel = voice_channel.name
+	# vc = ctx.message.author.voice.channel
+	# player = vc.create_ffmpeg_player('announce_well_done.mp3')
+	# player.start()
+	# while not player.is_done():
+		# await asyncio.sleep(1)
+	# player.stop()
+
 bot.run(token)
 
